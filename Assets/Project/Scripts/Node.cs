0381c3847fde3151e457554de8bb2c3717f94207
Avin19 <avinashy1993@gmail.com>
2024-08-02 22:18:38 +0530

    NodeElements

--- Assets/Project/Scripts/Node.cs
+++ Assets/Project/Scripts/Node.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 /// <summary>
 ///  
@@ -10,9 +11,25 @@ public class Node : MonoBehaviour
   private int rotationState = 0; // 0,1,2,3 represting 0 ,90 ,180 ,270 degree 
   private const int maxRotationState = 4;
 
-  [SerializeField] private bool[] connectons = new bool[4]; // Top , Right, Bottom , Left
+  [SerializeField] private bool connectons = false; // Top , Right, Bottom , Left
+  private SpriteRenderer spriteRenderer;
+  private int correctRotation = 0;
+  [SerializeField] private Material glowMaterial;
+  [SerializeField] private Material normalMaterial;
 
+  private void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    RandomPipeRotation();
+  }
+
+  private void RandomPipeRotation()
+  {
  
+    rotationState = Random.Range(1,3) % maxRotationState;
+    transform.rotation = Quaternion.Euler(0, 0, rotationState *90);
+  }
+
   private void OnMouseDown()
   {
     RotateNode();
@@ -22,7 +39,21 @@ public class Node : MonoBehaviour
   {
     rotationState = (rotationState + 1) % maxRotationState;
     transform.rotation = Quaternion.Euler(0, 0, rotationState *90);
+ 
+  }
+
+  private void Update()
+  {
     
+    if (correctRotation == transform.rotation.z)
+    {
+      spriteRenderer.sharedMaterial = glowMaterial;
+    }
+    else
+    {
+      spriteRenderer.sharedMaterial = normalMaterial;
+    }
+   
   }
 }
 
